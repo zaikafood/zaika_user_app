a3d4b61ff03d403565a7edc2bbccb6cf373aefc5
zaikafood <info@zaika.ltd>
2025-09-19 16:37:56 +0530

    staging final

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,19 +1,11 @@
 enum Environment { STAGING, PRODUCTION }
 
 class AppConfig {
-  // Yeh variable environment status ko hold karega (staging ya production)
   final Environment environment;
-
-  // Yeh aapka base URL hold karega
   final String baseUrl;
-
-  // Constructor
   AppConfig({required this.environment, required this.baseUrl});
 
-  // Yeh ek static variable hai jo AppConfig ka instance store karega
   static AppConfig? _instance;
-
-  // Jab app shuru hoga, to hum is function ko call karke environment set karenge
   static void init(Environment env) {
     if (_instance == null) {
       String baseUrl;
@@ -22,7 +14,6 @@ class AppConfig {
           baseUrl = 'https://staging.zaika.ltd';
           break;
         case Environment.PRODUCTION:
-          // TODO: Yahan apna production URL daalein
           baseUrl = 'https://zaika.ltd';
           break;
       }
